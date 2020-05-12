e71a8540a464f5d4c0a54bfd6bfc048193c37390
Justice Eziefule <eziefulejustice@gmail.com>
2020-05-12 21:34:59 +0100

    add JWT to HTTP hrader

---
+++ src/store/actions/messages.js
@@ -0,0 +1,19 @@
+import { apiCall } from '../../services/api';
+import { LOAD_MESSAGES, REMOVE_MESSAGE } from '../actionTypes';
+import { addError, removeError } from './errors';
+
+export const loadMessages = messages => ({
+    type: LOAD_MESSAGES,
+    messages
+});
+
+export const fetchMessages = () => {
+    return dispatch => {
+        return apiCall("get", '/api/messages').then((res) =>
+            dispatch(loadMessages(res))
+                .catch(err => {
+                    addError(err.message)
+                })
+        )
+    }
+}
---
+++ src/store/reducers/messages.js
@@ -0,0 +1,14 @@
+import {LOAD_MESSAGES, REMOVE_MESSAGE} from '../actionTypes';
+
+const messages = (state = [], action) => {
+    switch (action.type) {
+        case LOAD_MESSAGES:
+            return [...action.messages];
+        // case REMOVE_MESSAGE:
+    
+        default:
+            return state;
+    }
+}
+
+export default messages;
